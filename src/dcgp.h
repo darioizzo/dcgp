df7d3a1568306a7f62432723a81cd3a47a95339c
Dario Izzo <dario.izzo@gmail.com>
2015-07-11 18:01:39 +0200

    draft of header structure

---
+++ src/dcgp.h
@@ -0,0 +1,15 @@
+#ifndef DCGP_DCGP_H
+#define DCGP_DCGP_H
+
+#include "basis_function.h"
+
+namespace dcgp {
+
+class dcpg {
+public:
+    dcgp(unsigned int n, unsigned int m, unsigned int c, unsigned int r, unsigned int l, std::vector<basis_function>)
+};
+
+} // end of namespace dcgp
+
+#endif // DCGP_DCGP_H
