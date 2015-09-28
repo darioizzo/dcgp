07daa3ddef7b991e6c24b077ca0e72ea07406ad7
Dario Izzo <dario.izzo@gmail.com>
2015-09-28 16:05:45 +0200

    doxygen docs improved

--- src/basis_function.hpp
+++ src/basis_function.hpp
@@ -1,7 +1,8 @@
 #ifndef DCGP_BASIS_FUNCTION_H
 #define DCGP_BASIS_FUNCTION_H
 
-#include <functional>
+#include <functional> // std::function
+#include <utility>    // std::forward
 #include <string>
 #include <iostream>
 #include <vector>
@@ -9,61 +10,91 @@
 
 namespace dcgp {
 
-using namespace audi;
-
+/// Basic prototype of a kernel function for its simple evaluation
 using my_fun_type = std::function<double(const double&, const double&)>;
+/// Basic prototype of a kernel function for the evaluation of its Taylor expansion
 using d_my_fun_type = std::function<gdual(const gdual&, const gdual&)>;
+/// Basic prototype of a kernel function for the evaluation of its printable form
 using my_print_fun_type = std::function<std::string(std::string, std::string)>;
 
 /// Basis function
 /**
- * This struct represent a generic function (or expression) in d-CGP. It contains the std::function, whose type is
- * defined in my_fun_type, my_d_fun_type, my_print_fun_type, that allow to compute the function
- * value, its derivatives and its symbolic representation
- *
- * All functions that are represented in a d-CGP encoding must derive from this class and thus
- * the implementation of the function, its derivative and its symbolic representation must be
- * available as dcgp::my_fun_type, dcgp::my_d_fun_type and dcgp::my_print_fun_type in order
- * to be able to construct this object
+ * This struct represent one of the kernel functions to be used in a d-CGP expresion. 
+ * It contains three std::function, whose type are my_fun_type, my_d_fun_type,
+ * my_print_fun_type. These allow to compute the function value, its Taylor expansion
+ * and its symbolic representation.
  *
  * @author Dario Izzo (dario.izzo@gmail.com)
+ * @author Francesco Biscani (bluescarni@gmail.com)
  */
 struct basis_function
 {
     /// Constructor from std::function construction arguments
+    /*   
+     * Construct a function that can be used as a kernel in a d-CGP expression
+     *
+     * @param[in] f constructs a dcgp::my_fun_type 
+     * @param[in] df constructs a dcgp::d_my_fun_type 
+     * @param[in] pf constructs a dcgp::my_print_fun_type 
+     * @param[in] name string containing the function name (ex. "sum")
+     */
     template <typename T, typename U, typename V>
     basis_function(T &&f, U &&df, V&&pf, std::string name):m_f(std::forward<T>(f)), m_df(std::forward<U>(df)), m_pf(std::forward<V>(pf)), m_name(name) {}
 
-    /// Overload of operator(double, double)
+    /// Parenthesis operator overload (double)
     /**
-    * Allows to call a dcgp::basis_function with the syntax f(double x, double y) and get
-    * the function value in x,y in return
+    * Evaluates \f$f(x, y)\f$, that is the basis_function in a point \p x, \p y
+    *
+    * @param[in] x first input
+    * @param[in] y second input
+    *
+    * @return the function evaluated in \f$x,y\f$
     */
     double operator()(double x, double y) const
     {
             return m_f(x,y);
     }
 
-    /// Overload of operator(const gdual &, const gdual &)
+    /// Parenthesis operator overload (audi::gdual)
     /**
-    * Allows to call a dcgp::basis_function with the syntax f(const gdual & x, const gdual & y) and get
-    * the function Taylor expansion in x,y in return
+    * Computes, in the algebra of truncated polynomial, the function \f$f\f$. The
+    * result will be the Taylor expansion of the function \f$f\f$ composed with
+    * the functions \p p1 and \p p2
+    *
+    * @param[in] p1 first input
+    * @param[in] p2 second input
+    *
+    * @return the Taylor representation of \f$f\f$
     */
-    gdual operator()(const gdual & x, const gdual & y) const
+    audi::gdual operator()(const audi::gdual & p1, const audi::gdual & p2) const
     {
-            return m_df(x,y);
+            return m_df(p1,p2);
     }
 
-    /// Overload of operator(std::string, std::string)
+    /// Parenthesis operator overload (std::string)
     /**
-    * Allows to call a dcgp::basis_function with the syntax f(std::string x, std::string y) and get
-    * a symbolic representation of the function in the variables std::string x, std::string y
+    * Returns a symbolic representation for the operation made by \f$f\f$
+    *
+    * @param[in] s1 first input
+    * @param[in] s2 second input
+    *
+    * @return the string representation of the operation (ex. "ln(s1+s2)")
     */
-    std::string operator()(std::string x, std::string y) const
+    std::string operator()(std::string s1, std::string s2) const
     {
-            return m_pf(x,y);
+            return m_pf(s1,s2);
     }
 
+    /// Overloaded stream operator
+    /**
+     * Will stream the function name
+     * 
+     * @param[in,out] os target stream.
+     * @param[in] d dcgp::basis_function argument.
+     * 
+     * @return reference to \p os.
+     * 
+    */
     friend std::ostream& operator<<(std::ostream& os, const basis_function& d)
     {
         os << d.m_name;
@@ -79,7 +110,6 @@ struct basis_function
     /// Its name
     std::string m_name;
 };
-
 
 } // end of namespace dcgp
 
