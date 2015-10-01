bfd10775e90e86d8fa43541a567e9090bfb66532
Dario Izzo <dario.izzo@gmail.com>
2015-10-01 10:17:14 +0200

    perfromance test on std::function added

--- tests/function_calls_perf.cpp
+++ tests/function_calls_perf.cpp
@@ -1,29 +1,19 @@
-#include <iostream>
-#include <iomanip>
-#include <ctime>
-#include <random>
-#include "../src/dcgp.hpp"
 
+#define BOOST_TEST_MODULE dcgp_function_calls_perf
+#include <boost/test/unit_test.hpp>
+#include <boost/timer/timer.hpp>
+#include <vector>
 
+#include "../src/wrapped_functions.hpp"
+#include "../src/basis_function.hpp" //my_fun_type
+
+/*
 double my_fun_call(const std::vector<double>& a, const std::vector<double>& b, unsigned int N)
 {
     clock_t begin = clock();
     for (auto i = 0u; i < N; ++i)
     {
         dcgp::my_sum(a[i],b[i]);
-    }
-
-    clock_t end = clock();
-    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-    return elapsed_secs;
-}
-
-double d_my_fun_call(const std::vector<std::vector<double> >& a, const std::vector<std::vector<double> >& b, unsigned int N)
-{
-    clock_t begin = clock();
-    for (auto i = 0u; i < N; ++i)
-    {
-        dcgp::d_my_sum(a[i],b[i]);
     }
 
     clock_t end = clock();
@@ -44,48 +34,47 @@ double my_fun_call_indirect(const std::vector<double>& a, const std::vector<doub
     clock_t end = clock();
     double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
     return elapsed_secs;
-}
-
-double d_my_fun_call_indirect(const std::vector<std::vector<double> >& a, const std::vector<std::vector<double> >& b, unsigned int N)
-{
-    dcgp::function_set sum({"sum"});
-    dcgp::expression ex(1, 1, 3, 3, 3, sum(), 123);
-    clock_t begin = clock();
-    for (auto i = 0u; i < N; ++i)
-    {
-        ex.get_f()[0].m_df(a[i],b[i]);
-    }
-
-    clock_t end = clock();
-    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-    return elapsed_secs;
-}
+} */
 
 /// We test the speed of evauating sum(a+b) calling my_fun_type and my_d_fun_type 
-int main() {
-    // Number of evaluations tried
-    unsigned int N = 1000000;
+BOOST_AUTO_TEST_CASE(function_calls)
+{
+    // Number of evaluations to try
+    unsigned int N = 10000000;
+    // Random generators
+    std::random_device rd;
+    std::default_random_engine re(rd());
+
     // Generating the data set
     std::vector<double> a(N), b(N);
     std::vector<std::vector<double> > a_vector(N), b_vector(N);
-    std::default_random_engine re(123);
+
 
     for (auto j = 0u; j < N; ++j)
     {
         a[j] = std::uniform_real_distribution<double>(-1, 1)(re);
         b[j] = std::uniform_real_distribution<double>(-1, 1)(re);
-        a_vector[j] = {a[j]};
-        b_vector[j] = {b[j]};
+
     }
 
-    double time_my_fun=my_fun_call(a, b, N);
-    double time_my_fun_indirect=my_fun_call_indirect(a, b, N);
-    double time_d_my_fun=d_my_fun_call(a_vector, b_vector, N);
-    double time_d_my_fun_indirect=d_my_fun_call_indirect(a_vector, b_vector, N);
-    std::cout << "Cumulative time my_fun: " << time_my_fun << " [s]" << std::endl;
-    std::cout << "Cumulative time my_fun_indirect: " << time_my_fun_indirect << " [s]" << std::endl;
-    std::cout << "Cumulative time d_my_fun: " << time_d_my_fun << " [s]" << std::endl;
-    std::cout << "Cumulative time d_my_fun_indirect: " << time_d_my_fun_indirect << " [s]" << std::endl;
-    return 0;
+    // Starting the test
+    std::cout << "Testing " << N << " function calls to the sigmoid function" << std::endl;
+    {
+        boost::timer::auto_cpu_timer t; // Sets up a timer
+        for (auto i = 0u; i < N; ++i)
+        {
+            dcgp::my_sig(a[i],b[i]);
+        }
+    }
+
+    std::cout << "Testing " << N << " std::function calls to the sigmoid function" << std::endl;
+    dcgp::my_fun_type my_sig2(dcgp::my_sig<double>);
+    {
+        boost::timer::auto_cpu_timer t; // Sets up a timer
+        for (auto i = 0u; i < N; ++i)
+        {
+            my_sig2(a[i],b[i]);
+        }
+    }
 }
 
