2e5944703acdce5f4e420257b4b2ae222cabda67
Dario Izzo <dario.izzo@gmail.com>
2016-09-30 14:30:21 +0200

    one step forward to gdual exposition

--- dcgpy/core.cpp
+++ dcgpy/core.cpp
@@ -15,61 +15,69 @@ namespace py = pybind11;
 using namespace dcgp;
 using namespace dcgpy;
 
-using fun_type = std::function<double(const std::vector<double>&)>;
-using fun_print_type = std::function<std::string(const std::vector<std::string>&)>;
-
-PYBIND11_PLUGIN(_core) {
-    py::module m("dcgpy", "d-cgpy's core module");
-
-    py::class_<basis_function<double>>(m, "kernel")
+template <typename T>
+void expose_basis_function(const py::module &m, std::string name)
+{
+    std::string class_name = "kernel_" + name;
+    py::class_<basis_function<T>>(m, class_name.c_str())
     .def("__init__",
-        [](basis_function<double> &instance, const py::object &obj1, const py::object &obj2, const std::string &name)
+        [](basis_function<T> &instance, const py::object &obj1, const py::object &obj2, const std::string &name)
             {
-                fun_type my_function = [obj1](const std::vector<double>& x) { return obj1(x).cast<double>();};
-                fun_print_type my_print_function = [obj2](const std::vector<std::string>& x) { return obj2(x).cast<std::string>();};
-                new (&instance) basis_function<double>(my_function, my_print_function, name);
+                std::function<T(const std::vector<T>&)> my_function = [obj1](const  std::vector<T>& x) { return obj1(x).template cast<T>();};
+                std::function<std::string(const std::vector<std::string>&)> my_print_function = [obj2](const std::vector<std::string>& x) { return obj2(x).cast<std::string>();};
+                new (&instance) basis_function<T>(my_function, my_print_function, name);
             },
-        basis_function_init_doc().c_str(),
+        basis_function_init_doc(name).c_str(),
         py::arg("callable_f"),
         py::arg("callable_s"),
         py::arg("name")
         )
     .def("__call__",
-        [](basis_function<double> &instance, const std::vector<double> in)
+        [](basis_function<T> &instance, const std::vector<T> &in)
         {
             return instance(in);
         }
     )
     .def("__call__",
-        [](basis_function<double> &instance, const std::vector<std::string> in)
+        [](basis_function<T> &instance, const std::vector<std::string> &in)
         {
             return instance(in);
         }
     )
     .def("__repr__",
-        [](const basis_function<double> &instance) -> std::string
+        [](const basis_function<T> &instance) -> std::string
         {
             std::ostringstream oss;
             oss << instance;
             return oss.str();
         }
     );
+}
 
-    py::class_<function_set<double>>(m, "function_set")
+template <typename T>
+void expose_function_set(const py::module &m, std::string name)
+{
+    std::string class_name = "function_set_" + name;
+    py::class_<function_set<T>>(m, class_name.c_str())
     .def(py::init<>())
     .def(py::init<const std::vector<std::string>&>(),
         function_set_init_doc().c_str(),
         py::arg("kernels")
     )
     .def("__call__",
-        [](function_set<double> &instance)
+        [](function_set<T> &instance)
         {
             return instance();
         }
     );
+}
 
-    py::class_<expression<double>>(m, "expression")
-    .def(py::init<unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, std::vector<basis_function<double>>, unsigned int>(),
+template <typename T>
+void expose_expression(const py::module &m, std::string name)
+{
+    std::string class_name = "expression" + name;
+    py::class_<expression<T>>(m, class_name.c_str())
+    .def(py::init<unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, std::vector<basis_function<T>>, unsigned int>(),
         expression_init_doc().c_str(),
         py::arg("in"),
         py::arg("out"),
@@ -81,7 +89,7 @@ PYBIND11_PLUGIN(_core) {
         py::arg("seed")
     )
     .def("__repr__",
-        [](const expression<double> &instance) -> std::string
+        [](const expression<T> &instance) -> std::string
         {
             std::ostringstream oss;
             oss << instance;
@@ -89,32 +97,44 @@ PYBIND11_PLUGIN(_core) {
         }
     )
     .def("__call__",
-        [](const expression<double> &instance, std::vector<double> in)
+        [](const expression<T> &instance, const std::vector<T> &in)
         {
             return instance(in);
         }
     )
     .def("__call__",
-        [](const expression<double> &instance, std::vector<std::string> in)
+        [](const expression<T> &instance, const std::vector<std::string> &in)
         {
             return instance(in);
         }
     )
-    .def("set", &expression<double>::set, "Sets the expression chromosme", py::arg("chromosme"))
-    .def("get", &expression<double>::get, "Gets the expression chromosme")
-    .def("get_lb_", &expression<double>::get_lb, "Gets the lower bounds of the chromosome")
-    .def("get_ub", &expression<double>::get_ub, "Gets the upper bounds of the chromosome")
-    .def("get_active_genes", &expression<double>::get_active_genes, "Gets the idx of the active genes in the current chromosome (numbering is from 0)")
-    .def("get_active_nodes", &expression<double>::get_active_nodes, "Gets the idx of the active nodes in the current chromosome.")
-    .def("get_n", &expression<double>::get_n, "Gets the number of inputs of the c_CGP expression")
-    .def("get_m", &expression<double>::get_ub, "Gets the number of outputs of the c_CGP expression")
-    .def("get_f", &expression<double>::get_f, "Gets the kernel functions")
-    .def("mutate", (void (expression<double>::*)(unsigned int)) &expression<double>::mutate, "Mutates the selected gene within its allowed bounds.", py::arg("idx"))
-    .def("mutate", (void (expression<double>::*)(std::vector<unsigned int>)) &expression<double>::mutate, "Mutates the selected genes within its allowed bounds.", py::arg("idxs"))
-    .def("mutate_active", &expression<double>::mutate_active, "Mutates N randomly selected active genes within its allowed bounds.")
-    .def("mutate_active_cgene", &expression<double>::mutate_active_cgene, "Mutates exactly one randomly selected active connection genes within its allowed bounds.")
-    .def("mutate_ogene", &expression<double>::mutate_ogene, "Mutates exactly one of the output genes within its allowed bounds.")
-    .def("mutate_active_fgene", &expression<double>::mutate_active_fgene, "Mutates exactly one randomly selected active function genes within its allowed bounds.");
+    .def("set", &expression<T>::set, "Sets the expression chromosme", py::arg("chromosme"))
+    .def("get", &expression<T>::get, "Gets the expression chromosme")
+    .def("get_lb_", &expression<T>::get_lb, "Gets the lower bounds of the chromosome")
+    .def("get_ub", &expression<T>::get_ub, "Gets the upper bounds of the chromosome")
+    .def("get_active_genes", &expression<T>::get_active_genes, "Gets the idx of the active genes in the current chromosome (numbering is from 0)")
+    .def("get_active_nodes", &expression<T>::get_active_nodes, "Gets the idx of the active nodes in the current chromosome.")
+    .def("get_n", &expression<T>::get_n, "Gets the number of inputs of the c_CGP expression")
+    .def("get_m", &expression<T>::get_ub, "Gets the number of outputs of the c_CGP expression")
+    .def("get_f", &expression<T>::get_f, "Gets the kernel functions")
+    .def("mutate", (void (expression<T>::*)(unsigned int)) &expression<T>::mutate, "Mutates the selected gene within its allowed bounds.", py::arg("idx"))
+    .def("mutate", (void (expression<T>::*)(std::vector<unsigned int>)) &expression<T>::mutate, "Mutates the selected genes within its allowed bounds.", py::arg("idxs"))
+    .def("mutate_active", &expression<T>::mutate_active, "Mutates N randomly selected active genes within its allowed bounds.")
+    .def("mutate_active_cgene", &expression<T>::mutate_active_cgene, "Mutates exactly one randomly selected active connection genes within its allowed bounds.")
+    .def("mutate_ogene", &expression<T>::mutate_ogene, "Mutates exactly one of the output genes within its allowed bounds.")
+    .def("mutate_active_fgene", &expression<T>::mutate_active_fgene, "Mutates exactly one randomly selected active function genes within its allowed bounds.");
+
+}
+
+PYBIND11_PLUGIN(_core) {
+    py::module m("dcgpy", "d-cgpy's core module");
+
+    expose_basis_function<double>(m, "double");
+    expose_function_set<double>(m, "double");
+    expose_expression<double>(m, "double");
+    //expose_basis_function<gdual_d>(m, "gdual_d");
+    //expose_function_set<gdual_d>(m, "gdual_d");
+    expose_expression<gdual_d>(m, "gdual_d");
 
     return m.ptr();
 }
--- dcgpy/docstrings.hpp
+++ dcgpy/docstrings.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace dcgpy {
-std::string basis_function_init_doc();
+std::string basis_function_init_doc(const std::string &);
 std::string expression_init_doc();
 std::string function_set_init_doc();
 }
