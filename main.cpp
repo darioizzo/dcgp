8006c624185c81cd1c16039a9c7e2d32a137d0f5
Dario Izzo <dario.izzo@gmail.com>
2016-09-30 16:22:15 +0200

    still buggy

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ using fun_type = std::function<double(const std::vector<double>&)>;
 using fun_print_type = std::function<std::string(const std::vector<std::string>&)>;
 
 int main() {
-    fun_type my_cpp_callable1 = [](std::vector<double> x) { return x[0];};
-    fun_print_type my_cpp_callable2 = [](std::vector<std::string> x) { return x[0];};
-    auto a = basis_function<double>(my_cpp_callable1, my_cpp_callable2, "giggio");
+    expression<gdual_d> ex(1,1,1,6,6,2,function_set<gdual_d>({"sum", "diff", "div", "mul"})(), 67);
+    gdual_d x(1.2, "x", 5);
+    stream(std::cout, ex({x}), "\n");
 }
