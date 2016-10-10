d0c97fe344fe1099661a2f7d93304d1377b6eb15
Yawgmoth90 <yawgmoth90@gmail.com>
2016-10-10 15:47:33 +0200

    visualization first commit

--- dcgpy/__init__.py
+++ dcgpy/__init__.py
@@ -1,1 +1,6 @@
 from ._core import *
+
+from ._graphviz_visualize import _graphviz_visualize
+expression_double.visualize = _graphviz_visualize
+expression_gdual_double.visualize = _graphviz_visualize
+expression_gdual_vdouble.visualize = _graphviz_visualize
