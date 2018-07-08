2bc901b069b8e30f5f46d86b04676a7a9554410e
Dario Izzo <dario.izzo@gmail.com>
2018-07-08 09:41:13 +0200

    Appveyor CI attempt N.1

--- tools/install_appveyor.py
+++ tools/install_appveyor.py
@@ -1,5 +1,6 @@
 import os
 import re
+import sys
 
 
 def wget(url, out):
@@ -9,6 +10,7 @@ def wget(url, out):
 
 
 def rm_fr(path):
+    import os
     import shutil
     if os.path.isdir(path) and not os.path.islink(path):
         shutil.rmtree(path)
@@ -43,6 +45,17 @@ def run_command(raw_command, directory=None, verbose=True):
         raise RuntimeError(output)
     return output
 
+
+# Build type setup.
+BUILD_TYPE = os.environ['BUILD_TYPE']
+is_release_build = (os.environ['APPVEYOR_REPO_TAG'] == 'true') and bool(
+    re.match(r'v[0-9]+\.[0-9]+.*', os.environ['APPVEYOR_REPO_TAG_NAME']))
+if is_release_build:
+    print("Release build detected, tag is '" +
+          os.environ['APPVEYOR_REPO_TAG_NAME'] + "'")
+is_python_build = 'Python' in BUILD_TYPE
+
+
 # Get mingw and set the path.
 wget(r'https://github.com/bluescarni/binary_deps/raw/master/x86_64-6.2.0-release-posix-seh-rt_v5-rev1.7z', 'mw64.7z')
 run_command(r'7z x -oC:\\ mw64.7z', verbose=False)
@@ -53,51 +66,50 @@ os.environ['PATH'] = r'C:\\mingw64\\bin;' + os.environ['PATH']
 wget(r'https://github.com/bluescarni/binary_deps/raw/master/gmp_mingw_64.7z', 'gmp.7z')
 wget(r'https://github.com/bluescarni/binary_deps/raw/master/mpfr_mingw_64.7z', 'mpfr.7z')
 wget(r'https://github.com/bluescarni/binary_deps/raw/master/boost_mingw_64.7z', 'boost.7z')
+wget(r'https://github.com/bluescarni/binary_deps/raw/master/eigen3.7z', 'eigen3.7z')
+
 # Extract them.
 run_command(r'7z x -aoa -oC:\\ gmp.7z', verbose=False)
 run_command(r'7z x -aoa -oC:\\ mpfr.7z', verbose=False)
 run_command(r'7z x -aoa -oC:\\ boost.7z', verbose=False)
+run_command(r'7z x -aoa -oC:\\ eigen3.7z', verbose=False)
 
-# Download piranha 0.8 https://github.com/bluescarni/piranha/archive/v0.8.zip
-wget(r'https://github.com/bluescarni/piranha/archive/v0.8.zip', 'piranhav08.zip')
-run_command(r'unzip piranhav08.zip', verbose=False)
+
+# Download piranha 0.11 https://github.com/bluescarni/piranha/archive/v0.11.zip
+wget(r'https://github.com/bluescarni/piranha/archive/v0.11.zip', 'piranhav11.zip')
+run_command(r'unzip piranhav11.zip', verbose=False)
 # Move to the directory created and make piranha install its headers
-os.chdir('piranha-0.8')
+os.chdir('piranha-0.11')
 os.makedirs('build')
 os.chdir('build')
 print("Installing piranha")
-run_command(r'cmake -G "MinGW Makefiles" .. -DCMAKE_INSTALL_PREFIX=c:\\local ', verbose=False)
+run_command(
+    r'cmake -G "MinGW Makefiles" .. -DCMAKE_INSTALL_PREFIX=c:\\local ', verbose=False)
 run_command(r'mingw32-make install VERBOSE=1', verbose=False)
 os.chdir('../../')
 print("Piranha sucessfully installed .. continuing")
 
-# Download audi 1.0.3 https://github.com/darioizzo/audi/archive/v1.0.3.zip
-wget(r'https://github.com/darioizzo/audi/archive/v1.0.3.zip', 'audiv103.zip')
-run_command(r'unzip audiv103.zip', verbose=False)
+# Download audi 1.5 https://github.com/darioizzo/audi/archive/v1.5.zip
+wget(r'https://github.com/darioizzo/audi/archive/v1.5.zip', 'audiv15.zip')
+run_command(r'unzip audiv15.zip', verbose=False)
 # Move to the directory created and make audi install its headers
-os.chdir('audi-1.0.3')
+os.chdir('audi-1.5')
 os.makedirs('build')
 os.chdir('build')
 print("Installing audi")
-run_command(r'cmake -G "MinGW Makefiles" .. -DBUILD_TESTS=no -DCMAKE_INSTALL_PREFIX=c:\\local ', verbose=False)
+run_command(r'cmake -G "MinGW Makefiles" .. -DAUDI_BUILD_AUDI=yes -DAUDI_BUILD_PYAUDI=no -DAUDI_BUILD_TEST=no -DAUDI_WITH_MPPP=no -DCMAKE_INSTALL_PREFIX=c:\\local ', verbose=False)
 run_command(r'mingw32-make install VERBOSE=1', verbose=False)
 os.chdir('../../')
 print("Audi sucessfully installed .. continuing")
 
-# Set the path so that the precompiled libs can be found.
-os.environ['PATH'] = os.environ['PATH'] + r';c:\\local\\lib'
-
-# Build type setup.
-BUILD_TYPE = os.environ['BUILD_TYPE']
-is_release_build = (os.environ['APPVEYOR_REPO_TAG'] == 'true') and bool(re.match(r'v[0-9]+\.[0-9]+.*',os.environ['APPVEYOR_REPO_TAG_NAME']))
-if is_release_build:
-    print("Release build detected, tag is '" + os.environ['APPVEYOR_REPO_TAG_NAME'] + "'")
-is_python_build = 'Python' in BUILD_TYPE
+# Setup of the dependencies for a Python build.
 if is_python_build:
-    if BUILD_TYPE == 'Python35':
-        python_version = '35'
-    elif BUILD_TYPE == 'Python34':
+    if BUILD_TYPE == 'Python34':
         python_version = '34'
+    elif BUILD_TYPE == 'Python35':
+        python_version = '35'
+    elif BUILD_TYPE == 'Python36':
+        python_version = '36'
     elif BUILD_TYPE == 'Python27':
         python_version = '27'
     else:
@@ -123,32 +135,40 @@ if is_python_build:
     # Install pip and deps.
     wget(r'https://bootstrap.pypa.io/get-pip.py', 'get-pip.py')
     run_command(pinterp + ' get-pip.py')
-    run_command(pip + ' install pyaudi')
-    #run_command(pip + ' install mpmath')
     if is_release_build:
-        run_command(pip + ' install twine')
+        # call pip via python, workaround to avoid path issues when calling pip from win
+        # (https://github.com/pypa/pip/issues/1997)
+        run_command(pinterp + r' -m pip install twine')
+
+# Set the path so that the precompiled libs can be found.
+os.environ['PATH'] = os.environ['PATH'] + r';c:\\local\\lib'
 
 # Proceed to the build.
-os.makedirs('build')
-os.chdir('build')
-
-common_cmake_opts = r'-DCMAKE_PREFIX_PATH=c:\\local -DCMAKE_INSTALL_PREFIX=c:\\local -DBUILD_MAIN=no' #REMEMBER TO PUT THIS TO YES WHEN FINISHED
-
-# Configuration step.
+common_cmake_opts = r'-DCMAKE_PREFIX_PATH=c:\\local -DCMAKE_INSTALL_PREFIX=c:\\local'
 if is_python_build:
-    run_command(r'cmake -G "MinGW Makefiles" ..  -DDCGPY_INSTALL_PATH=c:\\local -DBUILD_TESTS=no -DBUILD_DCGPY=yes -DCMAKE_BUILD_TYPE=Release ' + common_cmake_opts + r' -DBoost_PYTHON_LIBRARY_RELEASE=c:\\local\\lib\\libboost_python' +
-                (python_version[0] if python_version[0] == '3' else r'') + r'-mgw62-mt-1_62.dll  -DPYTHON_EXECUTABLE=C:\\Python' + python_version + r'\\python.exe -DPYTHON_LIBRARY=C:\\Python' + python_version + r'\\libs\\python' + python_version + r'.dll')
+    os.makedirs('build_dcgp')
+    os.chdir('build_dcgp')
+    run_command(
+        r'cmake -G "MinGW Makefiles" ..  -DCMAKE_BUILD_TYPE=Release -DDCGP_BUILD_TESTS=no -DDCGP_BUILD_DCGP=yes -DDCGP_BUILD_DCGPY=no' + ' ' + common_cmake_opts)
+    run_command(r'mingw32-make install VERBOSE=1 -j2')
+    os.chdir('..')
+    os.makedirs('build_dcgpy')
+    os.chdir('build_dcgpy')
+    run_command(r'cmake -G "MinGW Makefiles" ..  -DDCGPY_INSTALL_PATH=c:\\local -DDCGP_BUILD_DCGP=no -DDCGP_BUILD_DCGPY=yes -DCMAKE_BUILD_TYPE=Release ' + common_cmake_opts + r' -DBoost_PYTHON_LIBRARY_RELEASE=c:\\local\\lib\\libboost_python' +
+                (python_version[0] if python_version[0] == '3' else r'') + r'-mgw62-mt-1_63.dll  -DPYTHON_EXECUTABLE=C:\\Python' + python_version + r'\\python.exe -DPYTHON_LIBRARY=C:\\Python' + python_version + r'\\libs\\python' + python_version + r'.dll')
+    run_command(r'mingw32-make install VERBOSE=1 -j2')
 elif BUILD_TYPE in ['Release', 'Debug']:
-    cmake_opts = r'-DCMAKE_BUILD_TYPE=' + BUILD_TYPE + r' -DBUILD_TESTS=yes ' + common_cmake_opts
+    os.makedirs('build_dcgp')
+    os.chdir('build_dcgp')
+    cmake_opts = r'-DCMAKE_BUILD_TYPE=' + BUILD_TYPE + \
+        r' -DDCGP_BUILD_TESTS=yes ' + common_cmake_opts
     run_command(r'cmake -G "MinGW Makefiles" .. ' + cmake_opts)
+    run_command(r'mingw32-make install VERBOSE=1 -j2')
+    run_command(r'ctest')
 else:
     raise RuntimeError('Unsupported build type: ' + BUILD_TYPE)
 
-# Build+install step.
-#run_command(r'cmake --build . --target install')
-run_command(r'mingw32-make install VERBOSE=1')
-
-# Testing, packaging.
+# Packaging.
 if is_python_build:
     # Run the Python tests.
     run_command(
@@ -163,18 +183,16 @@ if is_python_build:
         shutil.copy(_, 'dcgpy')
     run_command(pinterp + r' setup.py bdist_wheel')
     os.environ['PATH'] = ORIGINAL_PATH
-    run_command(pip + r' install dist\\' + os.listdir('dist')[0])
-
-    os.chdir('/')
+    # workaround necessary to be able to call pip install via python (next line) and
+    # not find a dcgpy already in the pythonpath
+    os.makedirs('garbage')
+    shutil.move('dcgpy', r'garbage')
+    shutil.move('dcgpy.egg-info', r'garbage')
+    # call pip via python, workaround to avoid path issues when calling pip from win
+    # (https://github.com/pypa/pip/issues/1997)
+    run_command(pinterp + r' -m pip install dist\\' + os.listdir('dist')[0])
     run_command(
-        pinterp + r' -c "from dcgpy import test; test.run_test_suite()"')
+        pinterp + r' -c "from dcgpy import test; test.run_test_suite()"', directory=r'c:\\')
     if is_release_build:
-        os.chdir('C:/projects/d-cgp/build/wheel')
         run_command(twine + r' upload -u darioizzo dist\\' +
                     os.listdir('dist')[0])
-elif BUILD_TYPE == 'Release':
-    run_command(r'ctest -VV')
-elif BUILD_TYPE == 'Debug':
-    run_command(r'ctest -VV')
-else:
-    raise RuntimeError('Unsupported build type: ' + BUILD_TYPE)
