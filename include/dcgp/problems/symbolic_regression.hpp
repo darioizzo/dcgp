3d0481ad0b72a13aaf1e38f05326085407b5a8cd
Dario Izzo <dario.izzo@gmail.com>
2019-09-17 15:56:16 +0200

    initial commit for the udp

---
+++ include/dcgp/problems/symbolic_regression.hpp
@@ -0,0 +1,21 @@
+#ifndef DCGP_SYMBOLIC_REGRESSION_H
+#ifndef DCGP_SYMBOLIC_REGRESSION_H
+
+#include <pagmo/types.hpp>
+
+namespace dcgp
+{
+class symbolic_regression
+{
+public:
+    pagmo::vector_double fitness(const vector_double &) const
+    {
+        return {1.};
+    }
+    std::pair<pagmo::vector_double, pagmo::vector_double> get_bounds() const
+    {
+        return {{0., 1.} {2., 3.}};
+    }
+}
+} // namespace dcgp
+#endif
