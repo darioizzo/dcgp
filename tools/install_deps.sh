109c7a6cd012ff90f69853b416fd1ef357024098
Dario Izzo <dario.izzo@gmail.com>
2019-09-19 11:28:21 +0200

    nth fix

--- tools/install_deps.sh
+++ tools/install_deps.sh
@@ -19,11 +19,11 @@ if [[ "${DCGP_BUILD}" != manylinux* ]]; then
 
     conda_pkgs="cmake eigen nlopt ipopt boost boost-cpp tbb tbb-devel pagmo audi"
 
-    #if [[ "${DCGP_BUILD}" == "Python37" || "${DCGP_BUILD}" == "OSXPython37" ]]; then
-    #    conda_pkgs="$conda_pkgs python=3.7 numpy cloudpickle dill ipyparallel numba pip"
-    #elif [[ "${DCGP_BUILD}" == "Python27" || "${DCGP_BUILD}" == "OSXPython27" ]]; then
-    #    conda_pkgs="$conda_pkgs python=2.7 numpy cloudpickle dill ipyparallel numba"
-    #fi
+    if [[ "${DCGP_BUILD}" == "Python37" || "${DCGP_BUILD}" == "OSXPython37" ]]; then
+        conda_pkgs="$conda_pkgs python=3.7 pyaudi"
+    elif [[ "${DCGP_BUILD}" == "Python27" || "${DCGP_BUILD}" == "OSXPython27" ]]; then
+        conda_pkgs="$conda_pkgs python=2.7 pyaudi"
+    fi
 
     # We create the conda environment and activate it
     conda create -q -p $deps_dir -y
