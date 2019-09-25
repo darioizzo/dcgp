eb4a9d411e0244519afa8d3adc2a86658fc8e0e0
Dario Izzo <dario.izzo@gmail.com>
2019-09-25 18:09:42 +0200

    data sets and code improvements

--- examples/supervised_learning_data_set_udp.cpp
+++ examples/supervised_learning_data_set_udp.cpp
@@ -14,16 +14,14 @@ int main()
 {
     // We read the data from file
     std::vector<std::vector<double>> X, Y;
-    read_data(X, Y, "../../examples/data/symbolic.data");
-    symbolic_regression udp(X, Y);
+    read_data(X, Y, "../../examples/data/kirby2.data");
+    symbolic_regression udp(X, Y, 2, 100, 101, 2);
     pagmo::problem prob{udp};
-    pagmo::print(prob);
     pagmo::population pop{prob, 4};
     dcgp::es4cgp uda(10000);
     pagmo::algorithm algo{uda};
-    pagmo::print(algo);
     algo.set_verbosity(100u);
     pop = algo.evolve(pop);
-    pagmo::print(pop);
+    pagmo::print(udp.pretty(pop.champion_x()), "\n");
     return false;
 }
