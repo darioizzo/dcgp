0ea33b81aabe31646c4dbf54a6597c44235743e4
Dario Izzo <dario.izzo@gmail.com>
2019-10-09 23:50:24 +0200

    gradient WIP

--- include/dcgp/algorithms/gd4cgp.hpp
+++ include/dcgp/algorithms/gd4cgp.hpp
@@ -3,9 +3,11 @@
 
 #include <audi/gdual.hpp>
 #include <numeric>
+#include <pagmo/algorithms/not_population_based.hpp>
 #include <pagmo/io.hpp>
 #include <pagmo/population.hpp>
 #include <stdexcept>
+
 #include <vector>
 
 #include <dcgp/kernel.hpp>
@@ -14,11 +16,11 @@
 
 namespace dcgp
 {
-class gd4cgp
+class gd4cgp : public pagmo::not_population_based
 {
 public:
-    /// Single entry of the log (iter, fevals, best, step_size)
-    typedef std::tuple<unsigned, unsigned long long, double, double> log_line_type;
+    /// Single entry of the log (iter, fevals, gevals, lr, best)
+    typedef std::tuple<unsigned, unsigned long long, unsigned long long, double, double> log_line_type;
     /// The log
     typedef std::vector<log_line_type> log_type;
 
@@ -48,11 +50,12 @@ public:
     pagmo::population evolve(pagmo::population pop) const
     {
         const auto &prob = pop.get_problem();
-        auto dim = prob.get_nx();
+        auto ncx = prob.get_nx() - prob.get_nix();
         auto n_obj = prob.get_nobj();
         const auto bounds = prob.get_bounds();
         auto NP = pop.size();
         auto fevals0 = prob.get_fevals(); // fevals already made
+        auto gevals0 = prob.get_gevals(); // gevals already made
         auto count = 1u;                  // regulates the screen output
 
         // PREAMBLE-------------------------------------------------------------------------------------------------
@@ -85,96 +88,51 @@ public:
         // No throws, all valid: we clear the logs
         m_log.clear();
 
-        // 1 - We make a copy of the cgp which we will use to create the gdual version
-        auto cgp = udp_ptr->get_cgp();
-        unsigned n_eph = static_cast<unsigned>(cgp.get_eph_val().size());
-        auto n_in = cgp.get_n() - n_eph;
-        auto n_out = cgp.get_m();
-        auto n_rows = cgp.get_r();
-        auto n_cols = cgp.get_c();
-        auto n_lb = cgp.get_l();
-        auto arity = cgp.get_arity();
-        auto eph_symb = cgp.get_eph_symb();
-        std::vector<kernel<double>> kernels_double = cgp.get_f();
-        kernel_set<audi::gdual_d> kernel_set;
-        for (const auto &f : kernels_double) {
-            kernel_set.push_back(f.get_name());
-        }
-        // The gdual version of the cgp (this will compute over the gdual algebra)
-        expression<gdual_d> dcgp(n_in, n_out, n_rows, n_cols, n_lb, arity, kernel_set(), n_eph);
-        dcgp.set_eph_symb(eph_symb); // for consistency
+        // 1 - We select the chromosome in the population.
+        auto sel_xf = select_individual(pop);
+        pagmo::vector_double x0(std::move(sel_xf.first)), fit0(std::move(sel_xf.second));
 
-        // 2 - We extract the best chromosome in the population.
-        auto best_idx = pop.best_idx();
-        auto best_x = pop.get_x()[best_idx];
-        double best_f = pop.get_f()[best_idx][0];
-        // ... and set the dCGP with it
-        std::vector<unsigned> best_xu(best_x.size() - n_eph);
-        std::transform(best_x.data() + n_eph, best_x.data() + best_x.size(), best_xu.begin(),
-                       [](double a) { return boost::numeric_cast<unsigned>(a); });
-        dcgp.set(best_xu);
-        std::vector<audi::gdual_d> eph_val;
-        for (decltype(n_eph) i = 0u; i < n_eph; ++i) {
-            eph_val.emplace_back(best_x[i], eph_symb[i], 1u); // Only first derivative is needed
-        }
-        dcgp.set_eph_val(eph_val);
-
-        // 3 - We Transform the points and labels into gduals as to be able to call loss
-        auto points = udp_ptr->get_points();
-        auto labels = udp_ptr->get_labels();
-        auto parallel_batches = udp_ptr->get_parallel_batches();
-        std::vector<std::vector<audi::gdual_d>> points_gdual;
-        std::vector<std::vector<audi::gdual_d>> labels_gdual;
-        for (const auto &point : points) {
-            std::vector<audi::gdual_d> point_gdual;
-            for (const auto &item : point) {
-                point_gdual.emplace_back(item);
-            }
-            points_gdual.push_back(point_gdual);
-        }
-        for (const auto &label : labels) {
-            std::vector<audi::gdual_d> label_gdual;
-            for (const auto &item : label) {
-                label_gdual.emplace_back(item);
-            }
-            labels_gdual.push_back(label_gdual);
-        }
-
-        // We create the symbol set of the differentials
-        std::vector<std::string> eph_symb_d; // symbol set needs to be with d added
-        for (const auto &symb : eph_symb) {
-            eph_symb_d.push_back("d" + symb);
-        }
-
-        // Gradient Descent iterations
+        // 2 - Gradient Descent iterations
         double lr = m_lr;
-        gdual_d loss;
 
-        for (unsigned i = 0u; i < 100; ++i) {
-            loss = dcgp.loss(points_gdual, labels_gdual, "MSE", parallel_batches);
-            loss.extend_symbol_set(eph_symb_d); // when the eph variables are not in the expression we still need their
-                                                // symbols as to get a derivative
-            audi::print(i, " ", loss.constant_cf(), "\n");
-            std::vector<double> loss_gradient(n_eph, 0.);
-            if (!(loss.get_order() == 0)) { // this happens when input terminals of the eph constants are inactive
-                                            // (gradient is then zero)
-                for (decltype(n_eph) i = 0u; i < n_eph; ++i) {
-                    std::vector<double> coeff(n_eph, 0.);
-                    coeff[i] = 1.;
-                    loss_gradient[i] = loss.get_derivative(coeff);
-                }
-            }
-            double loss_gradient_norm
-                = std::sqrt(std::inner_product(loss_gradient.begin(), loss_gradient.end(), loss_gradient.begin(), 0.));
-            // audi::print(loss_gradient, loss_gradient_norm);
+        if (m_verbosity > 0u) {
+            pagmo::print("\n", std::setw(7), "Iter:", std::setw(15), "Fevals:", std::setw(15), "Gevals:", std::setw(15),
+                         "lr:", std::setw(15), "Best:\n");
+            pagmo::print(std::setw(7), 0u, std::setw(15), prob.get_fevals() - fevals0, std::setw(15),
+                         prob.get_gevals() - gevals0, std::setw(15), lr, std::setw(15), fit0[0], '\n');
+            ++count;
+            // Logs
+            m_log.emplace_back(0u, prob.get_fevals() - fevals0, prob.get_gevals() - gevals0, lr, fit0[0]);
+        }
+
+        for (unsigned iter = 1u; iter <= m_max_iter; ++iter) {
+            auto grad = prob.gradient(x0);
+            auto fit0 = prob.fitness(x0);
+
+            double loss_gradient_norm = std::sqrt(std::inner_product(grad.begin(), grad.end(), grad.begin(), 0.));
             if (loss_gradient_norm == 0. || !std::isfinite(loss_gradient_norm)) { // nothing to do for GD
                 return pop;
             }
             // The SGD update rule
-            std::transform(loss_gradient.begin(), loss_gradient.end(), eph_val.begin(), eph_val.begin(),
-                           [lr, loss_gradient_norm](double a, gdual_d &b) { return b - a / loss_gradient_norm * lr; });
-
-            dcgp.set_eph_val(eph_val);
+            std::transform(grad.begin(), grad.end(), x0.data(), x0.data(),
+                           [lr, loss_gradient_norm](double a, double b) { return b - a / loss_gradient_norm * lr; });
+            // The log
+            if (m_verbosity > 0u) {
+                // Every m_verbosity generations print a log line
+                if (iter % m_verbosity == 1u || m_verbosity == 1u) {
+                    // Every 50 lines print the column names
+                    if (count % 50u == 1u) {
+                        pagmo::print("\n", std::setw(7), "Iter:", std::setw(15), "Fevals:", std::setw(15),
+                                     "Gevals:", std::setw(15), "lr:", std::setw(15), "Best:\n");
+                    }
+                    pagmo::print(std::setw(7), iter, std::setw(15), prob.get_fevals() - fevals0, std::setw(15),
+                                 prob.get_gevals() - gevals0, std::setw(15), lr, std::setw(15), fit0[0], '\n');
+                    ++count;
+                    // Logs
+                    m_log.emplace_back(iter, prob.get_fevals() - fevals0, prob.get_gevals() - gevals0, lr,
+                                       fit0[0]);
+                }
+            }
         }
 
         return pop;
