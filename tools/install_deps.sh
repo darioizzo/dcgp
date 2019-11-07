24ff3d2737c0778158566f0c5f8dd4a3e8178768
Dario Izzo <dario.izzo@gmail.com>
2019-11-07 15:32:15 +0100

    try azure on pr

--- tools/install_deps.sh
+++ tools/install_deps.sh
@@ -18,7 +18,7 @@ if [[ "${DCGP_BUILD}" != manylinux* ]]; then
     conda config --add channels conda-forge --force
 
     # obake-devel is needed as far as the conda package audi does not list it as a dependency
-    conda_pkgs="cmake eigen nlopt ipopt boost boost-cpp tbb tbb-devel pagmo audi symengine obake-devel"
+    conda_pkgs="cmake eigen boost boost-cpp tbb tbb-devel pagmo audi symengine obake-devel"
 
     if [[ "${DCGP_BUILD}" == "Python37" || "${DCGP_BUILD}" == "OSXPython37" ]]; then
         conda_pkgs="$conda_pkgs python=3.7 pyaudi pygmo"
