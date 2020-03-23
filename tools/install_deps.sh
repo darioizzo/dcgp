61f0f2240c72d4ee00f58079d9142fe8d1b0c7c7
Dario Izzo <dario.izzo@gmail.com>
2020-03-23 15:05:56 +0100

    deps cleaning

--- tools/install_deps.sh
+++ tools/install_deps.sh
@@ -18,7 +18,7 @@ if [[ "${DCGP_BUILD}" != manylinux* ]]; then
     conda config --add channels conda-forge --force
 
     # obake-devel is needed as far as the conda package audi does not list it as a dependency
-    conda_pkgs="cmake eigen boost boost-cpp tbb tbb-devel pagmo pagmo-devel audi symengine obake-devel cxx-compiler"
+    conda_pkgs="cmake eigen boost-cpp tbb-devel pagmo-devel audi symengine obake-devel cxx-compiler"
 
     if [[ "${DCGP_BUILD}" == "Python37" || "${DCGP_BUILD}" == "OSXPython37" ]]; then
         conda_pkgs="$conda_pkgs python=3.7 pyaudi pygmo"
@@ -28,7 +28,7 @@ if [[ "${DCGP_BUILD}" != manylinux* ]]; then
     conda create -q -p $deps_dir -y
     source activate $deps_dir
     conda install $conda_pkgs -y
-    
+
     # We install pybind11 from the specific commit needed to guarantee interoperability with pyaudi/pygmo
     export DCGPY_BUILD_DIR=`pwd`
     git clone https://github.com/pybind/pybind11.git
