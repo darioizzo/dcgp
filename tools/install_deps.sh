dd672e32cea3c888d2c95bd61c8e3f218f94b33c
Dario Izzo <dario.izzo@gmail.com>
2020-03-23 14:18:02 +0100

    attempt

--- tools/install_deps.sh
+++ tools/install_deps.sh
@@ -26,8 +26,23 @@ if [[ "${DCGP_BUILD}" != manylinux* ]]; then
 
     # We create the conda environment and activate it
     conda create -q -p $deps_dir -y
-    conda activate $deps_dir
+    source activate $deps_dir
     conda install $conda_pkgs -y
+    
+    # We install pybind11 from the specific commit needed to guarantee interoperability with pyaudi/pygmo
+    export DCGPY_BUILD_DIR=`pwd`
+    git clone https://github.com/pybind/pybind11.git
+    cd pybind11
+    git checkout 4f72ef846fe8453596230ac285eeaa0ce3278bb4
+    mkdir build
+    cd build
+    cmake \
+        -DPYBIND11_TEST=NO \
+        -DCMAKE_INSTALL_PREFIX=$DCGPY_BUILD_DIR \
+        -DCMAKE_PREFIX_PATH=$DCGPY_BUILD_DIR \
+        ..
+    make install
+    cd ../..
 fi
 
 set +e
