ad987d3113a2e58a2c845ecf1fb34dc1001a66a9
Dario Izzo <dario.izzo@gmail.com>
2021-07-14 20:32:52 +0200

    just CI

--- tools/gha_windows_MSVC2019_python.ps1
+++ tools/gha_windows_MSVC2019_python.ps1
@@ -2,8 +2,18 @@
 
 # Install conda environment
 conda config --set always_yes yes
-conda create --name dcgpy cmake boost-cpp eigen pagmo-devel tbb audi symengine obake-devel cxx-compiler pybind11 python=3.8 sympy matplotlib clang ninja
+conda create --name dcgpy cmake boost-cpp eigen pagmo-devel tbb audi symengine obake-devel cxx-compiler pybind11 python=3.8 sympy matplotlib clang ninja pygmo pyaudi
 conda activate dcgpy
+
+# Define environment variables for clang ...
+# ... and make them persistent 
+cmd.exe /c "call `"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat`" && set > %temp%\vcvars.txt"
+
+Get-Content "$env:temp\vcvars.txt" | Foreach-Object {
+  if ($_ -match "^(.*?)=(.*)$") {
+    Set-Content "env:\$($matches[1])" $matches[2]
+  }
+}
 
 #build and run the dcgp tests
 mkdir build
@@ -35,3 +45,5 @@ cmake `
     -DDCGP_BUILD_DCGPY=yes ..
 
 cmake --build . --target install --config Release
+
+python -c "from dcgpy import test; test.run_test_suite(); import pygmo; pygmo.mp_island.shutdown_pool(); pygmo.mp_bfe.shutdown_pool()"
