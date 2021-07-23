6ea4dde42f0646e42c085720af7ef7641c890363
Dario Izzo <dario.izzo@gmail.com>
2021-07-23 12:42:08 +0200

    env variables reset in doc bash script

--- tools/gha_build_docs.sh
+++ tools/gha_build_docs.sh
@@ -6,7 +6,10 @@ set -x
 # Exit on error.
 set -e
 
+export deps_dir=$HOME/local
+export PATH="$HOME/miniconda/bin:$PATH"
 conda_extra_pkgs="sphinx breathe sphinxcontrib-napoleon nbsphinx doxygen sphinx_rtd_theme"
+source activate $deps_dir
 conda install $conda_extra_pkgs -y
 
 # Move to docs folder
